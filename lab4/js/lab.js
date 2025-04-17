8e6069070cd333e31daff7146236eb70d2ba6a9b
Erenhamster <erenhamster3@gmail.com>
2025-04-17 12:47:15 -0700

    lab4 updates

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Hangman
+// Author: Leslie Hernandez-Nava <leerhern@ucsc.edu>
+// Date: April 17th 2025
 
 // Constants
 
@@ -14,7 +14,14 @@ function myFunction(param1, param2) {
 
 function main() {
   console.log("Main function started.");
-  // the code that makes everything happen
+ //print hangman platform
+  //generate a random word from a predefined list
+      //print dashes for the number of letters in the word
+  //accept user guesses of letters
+    //if correct, print letter in correct space
+    //if not correct, print body parts in order: head, body, left arm, right arm, left leg, right leg, game over
+  //if game over, restart and generate a new word
+  //if user guesses word within six guesses, print “Congratulations!” and full body walking away
 }
 
 // let's get this party started
